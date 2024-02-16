5c1f6633cc8e9df50637911b427c8976f0f78b4f
unknown <kaurodrigo.joenck2@gmail.com>
2024-02-15 23:32:59 -0300

    router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import LoginView from '@/views/LoginView.vue'
 import MainPage from '@/views/MainPage.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
