0e6758598d036c0acf96332950992b6ba251306d
unknown <kaurodrigo.joenck2@gmail.com>
2024-02-15 23:25:10 -0300

    package.json

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
-import router from './router'
+import router from './router/index'
 
 const app = createApp(App)
 
